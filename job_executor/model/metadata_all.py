1bd524985ad4ee21d723be83241d7aa2dc2bb590
DanielElisenberg <daniel@elisenberg.com>
2022-07-04 10:24:21 +0200

    model_camelcase: updated metadata_all model

--- job_executor/model/metadata_all.py
+++ job_executor/model/metadata_all.py
@@ -1,20 +1,21 @@
 from typing import List, Union
-from pydantic import BaseModel, root_validator
+from pydantic import root_validator
 
+from job_executor.model.camelcase_model import CamelModel
 from job_executor.model import Metadata
 from job_executor.adapter import local_storage
 
 
-class DataStoreInfo(BaseModel):
+class DataStoreInfo(CamelModel):
     name: str
     label: str
     description: str
-    languageCode: str
+    language_code: str
 
 
-class MetadataAll(BaseModel):
-    dataStore: DataStoreInfo
-    dataStructures: List[Metadata]
+class MetadataAll(CamelModel):
+    data_store: DataStoreInfo
+    data_structures: List[Metadata]
     iter: int = 0
 
     def __iter__(self):
@@ -22,24 +23,25 @@ class MetadataAll(BaseModel):
         return self
 
     def __next__(self):
-        if self.iter < len(self.dataStructures):
+        if self.iter < len(self.data_structures):
             index = self.iter
             self.iter = self.iter + 1
-            return Metadata(**self.dataStructures[index].dict())
+            return Metadata(**self.data_structures[index].dict())
         else:
             raise StopIteration
 
     def get(self, dataset_name: str) -> Union[Metadata, None]:
-        for metadata in self.dataStructures:
+        for metadata in self.data_structures:
             if metadata.name == dataset_name:
                 return Metadata(**metadata.dict())
         return None
 
     def dict(self):
         return {
-            "dataStore": self.dataStore.dict(),
+            "dataStore": self.data_store.dict(by_alias=True),
             "dataStructures": [
-                metadata.dict() for metadata in self.dataStructures
+                metadata.dict(by_alias=True)
+                for metadata in self.data_structures
             ]
         }
 
@@ -58,16 +60,16 @@ class MetadataAllDraft(MetadataAll):
         )
 
     def remove(self, dataset_name: str):
-        self.dataStructures = [
-            metadata for metadata in self.dataStructures
+        self.data_structures = [
+            metadata for metadata in self.data_structures
             if metadata.name != dataset_name
         ]
         self._write_to_file()
 
     def remove_all(self):
-        self.dataStructures = []
+        self.data_structures = []
         self._write_to_file()
 
     def add(self, metadata: Metadata):
-        self.dataStructures.append(metadata)
+        self.data_structures.append(metadata)
         self._write_to_file()
