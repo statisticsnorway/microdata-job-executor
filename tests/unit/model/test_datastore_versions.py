b87a4230302fb9df9ef5c037b6825e45d4c2f39d
DanielElisenberg <daniel@elisenberg.com>
2022-07-04 10:24:01 +0200

    model_camelcase: updated datastore versions models

--- tests/unit/model/test_datastore_versions.py
+++ tests/unit/model/test_datastore_versions.py
@@ -47,19 +47,22 @@ DATASTORE_VERSIONS_PATH = f'{DATASTORE_DIR}/datastore_versions.json'
 
 def test_data_structure_update():
     data_structure_update = DataStructureUpdate(**DATA_STRUCTURE_UPDATE)
-    assert data_structure_update.dict() == DATA_STRUCTURE_UPDATE
+    assert data_structure_update.dict(by_alias=True) == DATA_STRUCTURE_UPDATE
 
 
 def test_datastore_version():
     datastore_version = DatastoreVersion(**DATASTORE_VERSION)
-    assert datastore_version.dict() == DATASTORE_VERSION
+    assert datastore_version.dict(by_alias=True) == DATASTORE_VERSION
 
 
 def test_draft_version():
     draft_version = DraftVersion()
-    assert draft_version.dict() == load_json(DRAFT_VERSION_PATH)
+    assert draft_version.dict(by_alias=True) == load_json(DRAFT_VERSION_PATH)
 
 
 def test_datastore_versions():
     datastore_versions = DatastoreVersions()
-    assert datastore_versions.dict() == load_json(DATASTORE_VERSIONS_PATH)
+    assert (
+        datastore_versions.dict(by_alias=True)
+        == load_json(DATASTORE_VERSIONS_PATH)
+    )
