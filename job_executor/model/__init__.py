acfc215cd498b01c6b3af7c4590e9a84a4686146
DanielElisenberg <daniel@elisenberg.com>
2022-07-05 08:52:31 +0200

    more_testing_and_refactor: restructure models

--- job_executor/model/__init__.py
+++ job_executor/model/__init__.py
@@ -1,16 +1,10 @@
-from job_executor.model.datastore_versions import (
-    DataStructureUpdate,
-    DatastoreVersion,
-    DatastoreVersions,
-    DraftVersion
-)
+from job_executor.model.datastore_versions import DatastoreVersions
+from job_executor.model.datastore_version import DatastoreVersion, DraftVersion
+from job_executor.model.data_structure_update import DataStructureUpdate
 from job_executor.model.metadata import Metadata
 from job_executor.model.datastore import Datastore
 from job_executor.model.job import Job
-from job_executor.model.metadata_all import (
-    MetadataAll,
-    MetadataAllDraft
-)
+from job_executor.model.metadata_all import MetadataAll, MetadataAllDraft
 
 
 __all__ = [
---
+++ job_executor/model/datastore_version.py
@@ -0,0 +1,125 @@
+from typing import List, Union, Tuple
+from datetime import datetime
+
+from pydantic import root_validator
+
+from job_executor.model.camelcase_model import CamelModel
+from job_executor.model.data_structure_update import DataStructureUpdate
+from job_executor.adapter import local_storage
+from job_executor.exception.exception import (
+    NoSuchDraftException
+)
+
+
+class DatastoreVersion(CamelModel):
+    version: str
+    description: str
+    release_time: int
+    language_code: str
+    update_type: str
+    data_structure_updates: List[DataStructureUpdate]
+
+    def validate_bump_manifesto(
+        self, bump_manifesto: 'DatastoreVersion'
+    ) -> bool:
+        pending_operations = [
+            update.dict() for update in self.data_structure_updates
+            if update.release_status != 'DRAFT'
+        ]
+        other_pending_operations = [
+            update.dict() for update in bump_manifesto.data_structure_updates
+            if update.release_status != 'DRAFT'
+        ]
+        if len(pending_operations) != len(other_pending_operations):
+            return False
+        for pending_operation in other_pending_operations:
+            if pending_operation not in pending_operations:
+                return False
+        return True
+
+    def __iter__(self):
+        return iter([
+            DataStructureUpdate(**update.dict(by_alias=True))
+            for update in self.data_structure_updates
+        ])
+
+    def _calculate_update_type(self):
+        pending_operations = [
+            update.operation for update in self.data_structure_updates
+            if update.release_status != 'DRAFT'
+        ]
+        if (
+            'CHANGE_DATA' in pending_operations or
+            'REMOVE' in pending_operations
+        ):
+            self.update_type = 'MAJOR'
+        elif 'ADD' in pending_operations:
+            self.update_type = 'MINOR'
+        elif 'PATCH_METADATA' in pending_operations:
+            self.update_type = 'PATCH'
+        else:
+            self.update_type = None
+
+    def get_dataset_release_status(
+        self, dataset_name: str
+    ) -> Union[str, None]:
+        for update in self.data_structure_updates:
+            if update.name == dataset_name:
+                return update.release_status
+        return None
+
+
+class DraftVersion(DatastoreVersion):
+
+    @root_validator(skip_on_failure=True, pre=True)
+    @classmethod
+    def read_file(cls, _):
+        return local_storage.get_draft_version()
+
+    def add(self, data_structure_update: DataStructureUpdate):
+        self.data_structure_updates.append(data_structure_update)
+        self._write_to_file()
+
+    def delete_draft(self, dataset_name: str) -> DataStructureUpdate:
+        deleted_draft = next(
+            update for update in self.data_structure_updates
+            if update.name == dataset_name
+        )
+        self.data_structure_updates = [
+            update for update in self.data_structure_updates
+            if update.name != dataset_name
+        ]
+        self._write_to_file()
+        return deleted_draft
+
+    def release_pending(self) -> Tuple[List[DataStructureUpdate], str]:
+        draft_updates = []
+        pending_updates = []
+        for update in self.data_structure_updates:
+            if update.release_status == 'DRAFT':
+                draft_updates.append(update)
+            else:
+                pending_updates.append(update)
+        update_type = self.update_type
+        self.data_structure_updates = draft_updates
+        self._calculate_update_type()
+        self._write_to_file()
+        return pending_updates, update_type
+
+    def _write_to_file(self):
+        self.release_time = (
+            (datetime.now() - datetime.utcfromtimestamp(0)).days
+        )
+        self._calculate_update_type()
+        local_storage.write_draft_version(self.dict(by_alias=True))
+
+    def set_draft_release_status(self, dataset_name: str, new_status: str):
+        dataset_update = next(
+            update for update in self.data_structure_updates
+            if update.name == dataset_name
+        )
+        if dataset_update is None:
+            raise NoSuchDraftException(f'No draft for dataset {dataset_name}')
+        dataset_update.set_release_status(new_status)
+        self._calculate_update_type()
+        self._write_to_file()
