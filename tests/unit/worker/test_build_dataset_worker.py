6a80e54be496f25c8032b0eda3ced40db83b9521
DanielElisenberg <daniel@elisenberg.com>
2022-08-02 14:39:34 +0200

    pseudonymization_step: add pipeline test

--- tests/unit/worker/test_build_dataset_worker.py
+++ tests/unit/worker/test_build_dataset_worker.py
@@ -8,6 +8,23 @@ DATASET_NAME = 'BOSTED'
 JOB_ID = '1234-1234-1234-1234'
 WORKING_DIR = os.environ['WORKING_DIR']
 JOB_SERVICE_URL = os.environ['JOB_SERVICE_URL']
+PSEUDONYM_SERVICE_URL = os.environ['PSEUDONYM_SERVICE_URL']
+IDENTIFIERS = [
+    '00000000000001', '00000000000002', '00000000000003', '00000000000004',
+    '00000000000005', '00000000000006', '00000000000007', '00000000000008',
+    '123123', '123124', '123125', '123126', '123127', '123128', '1231299',
+    '1231210', '1231211', '1231212', '1231213', '1231214', '5123123',
+    '5123124', '5123125', '5123126', '5123127', '5123128', '51231299',
+    '51231210', '51231211', '51231212', '51231213', '51231214', '6123123',
+    '6123124', '6123125', '6123126', '6123127', '6123128', '61231299',
+    '61231210', '61231211', '61231212', '61231213', '61231214', '7123123',
+    '7123124', '7123125', '7123126', '7123127', '7123128', '71231299',
+    '71231210', '71231211', '71231212', '71231213', '71231214'
+]
+PSEUDONYM_DICT = {
+    identifier: str(index) for index, identifier in enumerate(IDENTIFIERS)
+}
+
 EXPECTED_REQUESTS_PARTITIONED = [
     {
         'json': {'status': 'validating'},
@@ -22,11 +39,75 @@ EXPECTED_REQUESTS_PARTITIONED = [
     {
         'json': {'status': 'transforming'},
         'method': 'PUT',
-        'url': f'{JOB_SERVICE_URL}/jobs/{JOB_ID}'},
+        'url': f'{JOB_SERVICE_URL}/jobs/{JOB_ID}'
+    },
+    {
+        'json': {'status': 'pseudonymizing'},
+        'method': 'PUT',
+        'url': f'{JOB_SERVICE_URL}/jobs/{JOB_ID}'
+    },
+    {
+        'json': [
+            '51231213',
+            '71231213',
+            '7123127',
+            '71231214',
+            '6123127',
+            '123124',
+            '5123125',
+            '5123126',
+            '51231211',
+            '51231299',
+            '7123125',
+            '123125',
+            '6123124',
+            '71231299',
+            '61231212',
+            '61231214',
+            '7123124',
+            '7123126',
+            '5123124',
+            '61231210',
+            '5123128',
+            '6123125',
+            '51231212',
+            '1231212',
+            '123126',
+            '123128',
+            '1231213',
+            '6123126',
+            '71231211',
+            '1231210',
+            '1231214',
+            '61231211',
+            '123123',
+            '6123128',
+            '61231299',
+            '5123127',
+            '71231210',
+            '7123128',
+            '51231210',
+            '1231299',
+            '5123123',
+            '51231214',
+            '1231211',
+            '123127',
+            '6123123',
+            '7123123',
+            '71231212',
+            '61231213'
+        ],
+        'method': 'POST',
+        'url': (
+            'http://mock.pseudonym.service/'
+            '?unit_id_type=PERSON&job_id=1234-1234-1234-1234'
+        )
+    },
     {
         'json': {'status': 'enriching'},
         'method': 'PUT',
-        'url': f'{JOB_SERVICE_URL}/jobs/{JOB_ID}'},
+        'url': f'{JOB_SERVICE_URL}/jobs/{JOB_ID}'
+    },
     {
         'json': {'status': 'converting'},
         'method': 'PUT',
@@ -53,11 +134,35 @@ EXPECTED_REQUESTS_IMPORT = [
     {
         'json': {'status': 'transforming'},
         'method': 'PUT',
-        'url': f'{JOB_SERVICE_URL}/jobs/{JOB_ID}'},
+        'url': f'{JOB_SERVICE_URL}/jobs/{JOB_ID}'
+    },
+    {
+        'json': {'status': 'pseudonymizing'},
+        'method': 'PUT',
+        'url': f'{JOB_SERVICE_URL}/jobs/{JOB_ID}'
+    },
+    {
+        'json': [
+            '00000000000001',
+            '00000000000002',
+            '00000000000003',
+            '00000000000004',
+            '00000000000005',
+            '00000000000006',
+            '00000000000007',
+            '00000000000008'
+        ],
+        'method': 'POST',
+        'url': (
+            'http://mock.pseudonym.service/'
+            '?unit_id_type=PERSON&job_id=1234-1234-1234-1234'
+        )
+    },
     {
         'json': {'status': 'enriching'},
         'method': 'PUT',
-        'url': f'{JOB_SERVICE_URL}/jobs/{JOB_ID}'},
+        'url': f'{JOB_SERVICE_URL}/jobs/{JOB_ID}'
+    },
     {
         'json': {'status': 'converting'},
         'method': 'PUT',
@@ -90,7 +195,12 @@ def test_build_partitioned_dataset(requests_mock):
     requests_mock.put(
         f'{JOB_SERVICE_URL}/jobs/{JOB_ID}', json={"message": "OK"}
     )
+    requests_mock.post(
+        f'{PSEUDONYM_SERVICE_URL}?unit_id_type=PERSON&job_id={JOB_ID}',
+        json=PSEUDONYM_DICT
+    )
     run_worker(JOB_ID, PARTITIONED_DATASET_NAME)
+    print(os.listdir(f'{WORKING_DIR}'))
     assert os.path.isdir(
         f'{WORKING_DIR}/{PARTITIONED_DATASET_NAME}__DRAFT'
     )
@@ -105,14 +215,21 @@ def test_build_partitioned_dataset(requests_mock):
         }
         for req in requests_mock.request_history
     ]
-    assert requests_made == EXPECTED_REQUESTS_PARTITIONED
+    assert len(requests_made) == len(EXPECTED_REQUESTS_PARTITIONED)
+    for index, _ in enumerate(requests_made):
+        assert request_matches(
+            requests_made[index], EXPECTED_REQUESTS_PARTITIONED[index]
+        )
 
 
 def test_import(requests_mock):
     requests_mock.put(
         f'{JOB_SERVICE_URL}/jobs/{JOB_ID}', json={"message": "OK"}
     )
-
+    requests_mock.post(
+        f'{PSEUDONYM_SERVICE_URL}?unit_id_type=PERSON&job_id={JOB_ID}',
+        json=PSEUDONYM_DICT
+    )
     run_worker(JOB_ID, DATASET_NAME)
 
     assert os.path.isfile(
@@ -129,4 +246,26 @@ def test_import(requests_mock):
         }
         for req in requests_mock.request_history
     ]
-    assert requests_made == EXPECTED_REQUESTS_IMPORT
+    assert len(requests_made) == len(EXPECTED_REQUESTS_IMPORT)
+    for index, _ in enumerate(requests_made):
+        assert request_matches(
+            requests_made[index], EXPECTED_REQUESTS_IMPORT[index]
+        )
+
+
+def request_matches(request: dict, other: dict):
+    if request.get('url') != other.get('url'):
+        return False
+    if request.get('method') != other.get('method'):
+        return False
+    request_payload = request.get('json')
+    other_payload = request.get('json')
+    if (
+        isinstance(request_payload, list)
+        and isinstance(other_payload, list)
+    ):
+        request_payload.sort()
+        other_payload.sort()
+        if request_payload != other_payload:
+            return False
+    return True
