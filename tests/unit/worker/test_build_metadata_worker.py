724ec248799f035aa42f4459bc748d7431989db8
DanielElisenberg <Daniel@Elisenberg.com>
2022-11-18 13:07:46 +0100

    rollback_import_jobs: test import linter

--- tests/unit/worker/test_build_metadata_worker.py
+++ tests/unit/worker/test_build_metadata_worker.py
@@ -5,7 +5,7 @@ from multiprocessing import Queue
 from pathlib import Path
 
 from requests_mock import Mocker as RequestsMocker
-from test_util import get_file_list_from_dir
+from tests.unit.test_util import get_file_list_from_dir
 
 from job_executor.adapter.local_storage import INPUT_DIR
 from job_executor.worker.build_metadata_worker import run_worker, local_storage
