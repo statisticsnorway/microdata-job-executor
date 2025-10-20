f9d5a70f4b76a2216309017c3e7c201c711d190b
DanielElisenberg <Daniel@Elisenberg.com>
2025-10-20 13:53:20 +0200

    file_system_adapters: update environment

--- tests/unit/config/test_environment.py
+++ tests/unit/config/test_environment.py
@@ -4,8 +4,6 @@ from job_executor.config import environment
 
 
 def test_config_from_environment():
-    assert environment.input_dir == os.environ.get("INPUT_DIR")
-    assert environment.working_dir == os.environ.get("WORKING_DIR")
     assert environment.datastore_dir == os.environ.get("DATASTORE_DIR")
     assert environment.pseudonym_service_url == (
         os.environ.get("PSEUDONYM_SERVICE_URL")
